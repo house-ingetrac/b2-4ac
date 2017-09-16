395501f98b02730a3e7c5026a9bf82f382a3bb5a
Bayan Berri <bberri@stuy.edu>
2017-09-16 16:52:09 -0400

    part 1 of homework 3 done

--- Occ.py
+++ Occ.py
@@ -4,16 +4,9 @@ SoftDev1 pd7
 HW03: StI/O: Divine your Destiny!
 2017-09-14
 '''
-import csv
 import random
 
 ## Separate all of the rows of the csv
-'''
-for every line in the file:
-make a new array/list .
-int(strname) will change the string to a number.
-then create the dictionary using index 0 of the array
-'''
 
 
 def makedict(filename):
@@ -21,23 +14,20 @@ def makedict(filename):
    # s = open(filename).read()
     #print s
     for line in open(filename):
-        if line[0]=='"':
-            ctr=1
-            while line[ctr]< len(line):
-                if line[ctr]!= '"':
-                    ctr+=1
-                else:
-                    d= dict(line[0:ctr+1],line [ctr+1:])
+        newline= line[line.rfind("n")]
+       # d=dict(,line[line.rfind(',')+1:len(line)+1])
+        d[line[0:line.rfind(',')]] = line[line.rfind(',')+1:len(line)-1]
+    #print d
+    return d            
+def make_float(d):
+    for k in d:
+        try:
+            d[k]=float(d[k])
+        except:
+            d[k]=d[k]
+    return d
 
-        else:
-            cntr=len(line)
-            while cntr!=0:
-                if line[cntr]==",":
-                    d=(dict(line[0:ctr],line[ctr:len(line)+1]))
-            ctr-=1
-    print d
-            
-makedict("occupations.csv")
+print make_float(makedict("occupations.csv"))
 
         
 
